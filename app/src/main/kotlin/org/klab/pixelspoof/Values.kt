600194e3a53e4fedd0b773adb180be08ba061912
karina-lab <81743527+karina-lab@users.noreply.github.com>
2026-04-11 13:38:13 +0300

    Add Values.kt

---
+++ app/src/main/kotlin/org/klab/pixelspoof/Values.kt
@@ -0,0 +1,71 @@
+package org.klab.pixelspoof
+
+object Values {
+    const val MODEL = "Pixel 10 Pro XL"
+    const val DEVICE = "mustang"
+    const val PRODUCT = "mustang"
+    const val BRAND = "google"
+    const val MANUFACTURER = "Google"
+    const val FINGERPRINT = "google/mustang/mustang:16/CP1A.260405.005/15001963:user/release-keys"
+    const val DESCRIPTION = "mustang-user 16 CP1A.260405.005 15001963 release-keys"
+    const val ID = "CP1A.260405.005"
+    const val BOOTLOADER = "deepspace-16.4-14791471"
+
+    const val SOC_MANUFACTURER = "Google"
+    const val SOC_MODEL = "Tensor G5"
+    const val PLATFORM = "laguna"
+
+    const val TIME = 1773134299000L
+    const val TIME_SEC = "1773134299"
+    const val TAGS = "release-keys"
+    const val TYPE = "user"
+    const val USER = "android-build"
+    const val HOST = "704b74c63ab8"
+
+    const val RELEASE = "16"
+    const val SDK_INT = 36
+    const val SDK_FULL = "36.1"
+    const val SECURITY_PATCH = "2026-04-05"
+    const val INCREMENTAL = "15001963"
+    const val FIRST_API_LEVEL = "36"
+
+    const val BUILD_DATE = "Tue Mar 10 09:18:19 UTC 2026"
+    const val BUILD_UUID = "-Y1LYKyFD2eSJBBIi73YKklZAh0xm8WiliiY6e8vlYg"
+    const val BASEBAND = "g5400i-251201-260127-B-14784805"
+
+    const val CLIENT_ID = "android-google"
+
+    val PIXEL_FEATURES = setOf(
+        "com.google.android.feature.PIXEL_EXPERIENCE",
+        "com.google.android.feature.TURBO_PRELOAD",
+        "com.google.android.feature.WELLBEING",
+        "com.google.android.feature.D2D_CABLE_MIGRATION_FEATURE",
+        "com.google.android.feature.PIXEL_2017_EXPERIENCE",
+        "com.google.android.feature.PIXEL_2018_EXPERIENCE",
+        "com.google.android.feature.PIXEL_2019_EXPERIENCE",
+        "com.google.android.feature.PIXEL_2019_MIDYEAR_EXPERIENCE",
+        "com.google.android.feature.PIXEL_2020_EXPERIENCE",
+        "com.google.android.feature.PIXEL_2020_MIDYEAR_EXPERIENCE",
+        "com.google.android.feature.PIXEL_2021_EXPERIENCE",
+        "com.google.android.feature.PIXEL_2021_MIDYEAR_EXPERIENCE",
+        "com.google.android.feature.PIXEL_2022_EXPERIENCE",
+        "com.google.android.feature.PIXEL_2022_MIDYEAR_EXPERIENCE",
+        "com.google.android.feature.PIXEL_2023_EXPERIENCE",
+        "com.google.android.feature.PIXEL_2023_MIDYEAR_EXPERIENCE",
+        "com.google.android.feature.PIXEL_2024_EXPERIENCE",
+        "com.google.android.feature.PIXEL_2024_MIDYEAR_EXPERIENCE",
+        "com.google.android.feature.PIXEL_2025_EXPERIENCE",
+        "com.google.android.feature.PIXEL_2025_MIDYEAR_EXPERIENCE",
+        "com.google.android.feature.GOOGLE_BUILD",
+        "com.google.android.feature.GOOGLE_EXPERIENCE",
+        "com.google.android.feature.GOOGLE_CAMERA_EXPERIENCE",
+        "com.google.android.feature.QUICK_TAP",
+        "com.google.android.feature.NOW_PLAYING_APP_26Q1",
+        "com.google.android.feature.NEXT_GENERATION_ASSISTANT",
+        "com.google.android.feature.GEMINI_EXPERIENCE",
+        "com.google.android.feature.AMBIENT_DATA",
+        "com.google.android.feature.CONTEXTUAL_SEARCH",
+        "com.google.android.feature.CONTEXTUAL_SEARCH_LIVE_TRANSLATE",
+        "com.android.systemui.SUPPORTS_DRAG_ASSISTANT_TO_SPLIT"
+    )
+}
