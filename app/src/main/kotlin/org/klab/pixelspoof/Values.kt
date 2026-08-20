30486638ab69bcdb6c7cefc1ba468dbbedac2462
karina-lab <81743527+karina-lab@users.noreply.github.com>
2026-08-20 22:14:21 +0300

    kodiak

--- app/src/main/kotlin/org/klab/pixelspoof/Values.kt
+++ app/src/main/kotlin/org/klab/pixelspoof/Values.kt
@@ -1,37 +1,37 @@
 package org.klab.pixelspoof
 
 object Values {
-    const val MODEL = "Pixel 10 Pro XL"
-    const val DEVICE = "mustang"
-    const val PRODUCT = "mustang"
+    const val MODEL = "Pixel 11 Pro XL"
+    const val DEVICE = "kodiak"
+    const val PRODUCT = "kodiak"
     const val BRAND = "google"
     const val MANUFACTURER = "Google"
-    const val FINGERPRINT = "google/mustang/mustang:17/CP2A.260805.005/15828068:user/release-keys"
-    const val DESCRIPTION = "mustang-user 17 CP2A.260805.005 15828068 release-keys"
-    const val ID = "CP2A.260805.005"
-    const val BOOTLOADER = "deepspace-17.2-15372054"
+    const val FINGERPRINT = "google/kodiak/kodiak:17/CD1A.260714.001.A9/15938155:user/release-keys"
+    const val DESCRIPTION = "kodiak-user 17 CD1A.260714.001.A9 15938155 release-keys"
+    const val ID = "CD1A.260714.001.A9"
+    const val BOOTLOADER = "spacecraft-17.4-15938155"
 
     const val SOC_MANUFACTURER = "Google"
-    const val SOC_MODEL = "Tensor G5"
-    const val PLATFORM = "laguna"
+    const val SOC_MODEL = "Tensor G6"
+    const val PLATFORM = "malibu"
 
-    const val TIME = 1783650420000L
-    const val TIME_SEC = "1783650420"
+    const val TIME = 1784919402000L
+    const val TIME_SEC = "1784919402"
     const val TAGS = "release-keys"
     const val TYPE = "user"
     const val USER = "android-build"
-    const val HOST = "e320c2924fdd"
+    const val HOST = "r-36404f8caf3535e4-26ss"
 
     const val RELEASE = "17"
     const val SDK_INT = 37
     const val SDK_FULL = "37.0"
     const val SECURITY_PATCH = "2026-08-05"
-    const val INCREMENTAL = "15828068"
-    const val FIRST_API_LEVEL = "36"
+    const val INCREMENTAL = "15938155"
+    const val FIRST_API_LEVEL = "37"
 
-    const val BUILD_DATE = "Thu Jul  9 19:27:00 PDT 2026"
-    const val BUILD_UUID = "MQutNG2cNDe1RMlYCrCstin4l-UgThb3JrfvXB5Z0o4"
-    const val BASEBAND = "g5400i-260317-260429-B-15308590"
+    const val BUILD_DATE = "Fri Jul 24 18:56:42 UTC 2026"
+    const val BUILD_UUID = "SOpdezEibqmfdT6p2_OfWNdeVDFSWI-NZSL-D5zp22I"
+    const val BASEBAND = "a900a-MP_260716-260716-M-15880348"
 
     const val CLIENT_ID = "android-google"
 
@@ -56,6 +56,8 @@ object Values {
         "com.google.android.feature.PIXEL_2024_MIDYEAR_EXPERIENCE",
         "com.google.android.feature.PIXEL_2025_EXPERIENCE",
         "com.google.android.feature.PIXEL_2025_MIDYEAR_EXPERIENCE",
+        "com.google.android.feature.PIXEL_2026_EXPERIENCE",
+        "com.google.android.feature.PIXEL_2026_MIDYEAR_EXPERIENCE",
         "com.google.android.feature.GOOGLE_BUILD",
         "com.google.android.feature.GOOGLE_EXPERIENCE",
         "com.google.android.feature.GOOGLE_CAMERA_EXPERIENCE",
@@ -63,6 +65,7 @@ object Values {
         "com.google.android.feature.NOW_PLAYING_APP_26Q1",
         "com.google.android.feature.NEXT_GENERATION_ASSISTANT",
         "com.google.android.feature.GEMINI_EXPERIENCE",
+        "com.google.android.feature.GEMINI_INTELLIGENCE_EXPERIENCE",
         "com.google.android.feature.AMBIENT_DATA",
         "com.google.android.feature.CONTEXTUAL_SEARCH",
         "com.google.android.feature.CONTEXTUAL_SEARCH_LIVE_TRANSLATE",
